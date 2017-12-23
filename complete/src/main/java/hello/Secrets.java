59138556b655d953e50183613be3e39a524b7b80
Chongzheng.Shi <10chongzheng@outlook.com>
2017-12-23 20:56:29 +0800

    a ad poster.Now i can do upload user visiting urls. Next: get a ad DOM return to the page~  Hei hei~

---
+++ complete/src/main/java/hello/Secrets.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class Secrets {
+
+    //    private final long id;
+    private final String url;
+
+    public Secrets(String url) {
+//        this.id = id;
+        this.url = url;
+    }
+
+//    public long getId() {
+//        return id;
+//    }
+
+    public String getUrl() {
+        return url;
+    }
+}
