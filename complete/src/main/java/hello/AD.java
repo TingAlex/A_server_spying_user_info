9f2d3659d43e6ad387a5072178a8450fb1e2b913
Chongzheng.Shi <10chongzheng@outlook.com>
2017-12-25 08:47:21 +0800

    a ad poster.Now i can do give a user a unique id and upload  user visiting urls, and return an ad iframe src. Hei hei~

---
+++ complete/src/main/java/hello/AD.java
@@ -0,0 +1,17 @@
+package hello;
+
+import java.util.UUID;
+
+public class AD {
+
+    private final String url;
+
+    public AD(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+}
--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    @RequestMapping("/receiveSecret")
-    public Secrets greeting(@RequestParam(value = "uid") String uid, @RequestParam(value = "secretUrl") String url) {
-        System.out.println(uid + " " + url);
-
-        return new Secrets(uid, url);
-    }
+//    @RequestMapping("/receiveSecret")
+//    public Secrets greeting(@RequestParam(value = "uid") String uid, @RequestParam(value = "secretUrl") String url) {
+//        System.out.println(uid + " " + url);
+//
+//        return new Secrets(uid, url);
+//    }
+@RequestMapping("/receiveSecret")
+public AD greeting(@RequestParam(value = "uid") String uid, @RequestParam(value = "secretUrl") String url) {
+    System.out.println(uid+url);
+    return new AD("https://forms.office.com/Pages/ResponsePage.aspx?id=oBzDhDusrk6tEVGdgCM-b2DJ_fQODH9HlYefNBh-r3tUMTdOMFdETzczMzc2UUU5TUxINllGS0ZHSi4u&embed=true");
+}
 
     @RequestMapping("/uid")
     public UID greeting() {
